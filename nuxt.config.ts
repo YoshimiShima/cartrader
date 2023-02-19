477e16247fb06fa2855c9e663818772c542dc84e
YoshimiShima <yoshimi-ramayana@hotmail.co.jp>
2023-02-19 20:36:01 +0900

    firstcommit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,31 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
+// // import { NuxtConfig } from '@nuxt/types'
+// //   const config: NuxtConfig = {
+// //     modules: ['@nuxtjs/apollo'],
+// //     plugins: ['@/plugins/apollo'],
+// //     apollo: {
+// //       clientConfigs: {
+// //         default: {
+// //           httpEndpoint: 'https://yoshimi-exam.hasura.app/v1/graphql',
+// //         }
+// //       }
+// //     }
+// //   }
 
-})
+// export default config
+
+  export default {
+    buildModules: ['@nuxtjs/apollo'],
+    apollo: {
+      clientConfigs: {
+        default: {
+          httpEndpoint: 'https://yoshimi-exam.hasura.app/v1/graphql',
+          httpLinkOptions: {
+            headers: {
+              'X-Hasura-Admin-Secret': 'EvL8VXyXwIEuTfVLlCWXh6azK3YvwHUbjipYlXo8ckIRZSfxa6cHmjiXnSX73svx',
+            },
+          },
+        }
+      }
+    }
+  }
---
+++ pages/posts.vue
@@ -0,0 +1,36 @@
+<template lang="pug">
+NConfigProvider
+  NMessageProvider
+    NNotificationProvider
+      h1 input form
+      ProvideData
+#usersQuery
+  ul
+    li(v-for='user in users', :key='user.id') {{ user.name }}
+
+</template>
+
+<script lang="ts" setup>
+import {
+  NConfigProvider,
+  NMessageProvider,
+  NNotificationProvider,
+} from 'naive-ui'
+  // @ts-ignore
+import ProvideData from '../components/ProvideData.vue'
+
+import { useQuery } from '@nuxtjs/apollo'
+import usersQuery from '../graphql/query.gql'
+
+const { data } = useQuery(usersQuery)
+const users = ref(data.users)
+
+
+
+</script>
+
+<style>
+body {
+  padding: 16px;
+}
+</style>
