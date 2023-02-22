a9ad52b4db0d1fca8a2427e839b3c8058cfe245d
YoshimiShima <yoshimi-ramayana@hotmail.co.jp>
2023-02-22 11:16:57 +0900

    postundefined

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -11,22 +11,22 @@ header
 .header-link {
   font-family: 'Courier New', Courier, monospace;
   color: gray;
-  font-size: 36px;
-  font-weight: 900;
+  font-size: 24px;
+  font-weight: 600;
   display: flex;
   grid-column-gap: 20px;
   padding: 20px;
 
-  background-color: cadetblue;
+  background-color: rgba(98, 163, 165, 0.7);
 }
 @media only screen and (max-width: 1000px) and (min-width: 768px) {
   .header-link {
-    font-size: 28px;
+    font-size: 16px;
   }
 }
 @media only screen and (max-width: 767px) {
   .header-link {
-    font-size: 12px;
+    font-size: 8px;
   }
 }
 .p {
--- pages/posts.vue
+++ pages/posts.vue
@@ -9,8 +9,8 @@ NConfigProvider
       #query
         ul
           li(v-for='(user,index) in users') {{ user.name }}
-        //- ul
-        //-   li(v-for='(post,index) in posts') {{ post.post }}
+        ul
+          li(v-for='(post,index) in posts') {{ post.post }}
 
 </template>
 
@@ -26,30 +26,30 @@ import ProvideData from '../components/ProvideData.vue'
 
 // import { useQuery } from '@nuxtjs/apollo'
 
-import query from '../apollo/UserQuery.gql'
-// import PostQuery from '../apollo/PostQuery.gql'
-
+import UserQuery from '../apollo/UserQuery.gql'
+import PostQuery from '../apollo/PostQuery.gql'
 
 const users = ref([])
-// const posts = ref([])
+const posts = ref([])
 
-// const load_data = (async() => {
-//   const { data: userData } = await useAsyncQuery(usersQuery)
-//   if(userData?.value?.users){
-//     users.value = userData.value.users
-//   }
-// })
 const load_data = (async() => {
-  const { data } = await useAsyncQuery(query)
-  if(data.value?.users){
+  const { data } = await useAsyncQuery(UserQuery, PostQuery)
+  if(data?.value?.users){
     users.value = data.value.users
   }
-})
-//   const { data: postData } = await useAsyncQuery(postsQuery)
-//   if(postData?.value?.posts){
-//     posts.value = postData.value.posts
+
+// const load_data = (async() => {
+//   const { data } = await useAsyncQuery(query)
+//   if(data.value?.users){
+//     users.value = data.value.users
 //   }
 // })
+  // const { data } = await useAsyncQuery(postQuery)
+  if(data?.value?.posts){
+    posts.value = data.value.posts
+  }
+})
+console.log(posts.post)
 
 </script>
 
@@ -57,4 +57,5 @@ const load_data = (async() => {
 body {
   padding: 16px;
 }
+
 </style>
