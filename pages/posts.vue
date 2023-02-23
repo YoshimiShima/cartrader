fc94f360a330d09e1e0225700bec0ad32efb3af2
YoshimiShima <yoshimi-ramayana@hotmail.co.jp>
2023-02-23 12:24:49 +0900

    mutation

--- pages/posts.vue
+++ pages/posts.vue
@@ -4,13 +4,55 @@ NConfigProvider
     NNotificationProvider
       h1 input form
       ProvideData
+#query
+  button(@click='load_data()') query
+  button(@click='isShowEditArea = true') new user
+  tr
+    th ID
+    th name
+    th age
+    th gender
+    th occupation
+    th address
+    th post
+    th edit
+  tr(v-for='(user,index) in users')
+    td {{ user.id }}
+    td {{ user.name }}
+    td {{ user.age }}
+    td {{ user.gender }}
+    td {{ user.occupation }}
+    td {{ user.address }}
+    tr(v-for='(post,index) in user.posts')
+      td {{ post.post }}
+    td
+      button(@click="editUser(user)") edit
+      button(@click="deleteUser(user)") delete
 
-      button(@click='load_data()') query
-      #query
-        ul
-          li(v-for='(user,index) in users') {{ user.id }} : {{ user.name }}
-        ul
-          li(v-for='(post,index) in posts') {{ post.user_id }} : {{ post.post }}
+  div(v-if='isShowEditArea')
+    h2 {{ formTitle }}
+    div
+      input(v-model='editedUser.name', type='text', placeholder='name')
+    div
+      input(v-model='editedUser.age', type='number', placeholder='age')
+    div
+      input(v-model='editedUser.gender', type='text', placeholder='gender')
+    div
+      input(v-model='editedUser.occupation', type='text', placeholder='occupation')
+    div
+      input(v-model='editedUser.address', type='text', placeholder='address')
+    div
+      textarea(v-model='editedUser.posts.post', cols="30", rows="10", type='text', placeholder='post')
+    div
+      button(@click='close') quit
+      button(@click='save') save
+
+
+        //- ul
+        //-   li(v-for='(user,index) in users') {{ user.id }} : {{ user.name }}
+        //- ul
+        //-   li(v-for='(post,index) in posts') {{ post.user_id }} : {{ post.post }}
+
 
 </template>
 
@@ -25,24 +67,52 @@ import {
 import ProvideData from '../components/ProvideData.vue'
 
 // import { useQuery } from '@nuxtjs/apollo'
+import { defineComponent } from 'vue';
 
 import UserQuery from '../apollo/UserQuery.gql'
 import PostQuery from '../apollo/PostQuery.gql'
+import InsertUser from '../apollo/InsertUser.gql'
 
 const users = ref([])
 const posts = ref([])
 
-// const load_data = (async() => {
-//   const userData = await useAsyncQuery(UserQuery)
-//   if(userData?.value?.users){
-//     users.value = userData.value.users
-//   }
+interface User {
+  id: number | null;
+  name: string | null;
+  age: number | null;
+  gender: string | null;
+  address: string | null;
+  occupation: string | null;
+  post: string | null;
+}
 
-//   const postData = await useAsyncQuery(PostQuery)
-//   if(postData?.value?.posts){
-//     posts.value = postData.value.posts
-//   }
-// })
+const defaultEditedUser: User = {
+  id: null,
+  name: null,
+  age: null,
+  gender: null,
+  address: null,
+  occupation: null,
+  post: null
+};
+
+const App = defineComponent({
+  data() {
+    return {
+      users: [] as User[],
+      isShowEditArea: false,
+      editedIndex: -1,
+      editedUser: { ...defaultEditedUser },
+      defaultEditedUser,
+    };
+  },
+  computed: {
+    formTitle(): string {
+      return this.editedIndex === -1 ? 'new user input' : 'edit user';
+    },
+  },
+});
+
 const load_data = (async() => {
   const userData = await useAsyncQuery(UserQuery)
   if(userData.data.value?.users){
@@ -52,7 +122,29 @@ const load_data = (async() => {
   if(postData.data.value?.posts){
     posts.value = postData.data.value.posts
   }
+  console.log(users.posts.post)
 })
+
+const variables = ref({
+  name: '',
+  age: '',
+  gender: '',
+  occupation: '',
+  address: '',
+  posts: '',
+});
+
+const { mutate } = useMutation(InsertUser, { variables });
+  (async () => {
+    try {
+      const { data } = await mutate();
+      console.log('Mutation result:', data);
+    } catch (error) {
+      console.error(error);
+      }
+  })();
+
+
 
 
 </script>
@@ -60,6 +152,9 @@ const load_data = (async() => {
 <style>
 body {
   padding: 16px;
+}
+tr {
+grid-auto-flow: 10px;
 }
 
 </style>
